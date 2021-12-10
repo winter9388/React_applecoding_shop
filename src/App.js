abd0ba4638b8daf8100f0d01f26560eb62bac18d
winter9388 <winter9388@naver.com>
2021-12-11 00:52:52 +0900

    Make shoes card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 /* eslint-disable */
-
+import React, { useState } from "react";
+import Data from "./data.js";
 import "./App.css";
 
 function App() {
+	let [shoe, setShoe] = useState(Data);
+
 	return (
 		<div className="App">
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -40,9 +43,7 @@ function App() {
 
 			<div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
 				<div class="col-md-6 px-0">
-					<h1 class="display-4 fst-italic">
-						Title of a longer featured blog post
-					</h1>
+					<h1 class="display-4 fst-italic">20% Season OFF</h1>
 					<p class="lead my-3">
 						Multiple lines of text that form the lede, informing new readers
 						quickly and efficiently about what’s most interesting in this post’s
@@ -58,32 +59,38 @@ function App() {
 
 			<div className="container">
 				<div className="row">
-					<div className="col-md-4">
-						<img
-							src="https://codingapple1.github.io/shop/shoes1.jpg"
-							width="100%"
-						></img>
-						<h4>상품명</h4>
-						<p>상품 설명 & 가격</p>
-					</div>
-					<div className="col-md-4">
-						<img
-							src="https://codingapple1.github.io/shop/shoes2.jpg"
-							width="100%"
-						></img>
-						<h4>상품명</h4>
-						<p>상품 설명 & 가격</p>
-					</div>
-					<div className="col-md-4">
-						<img
-							src="https://codingapple1.github.io/shop/shoes3.jpg"
-							width="100%"
-						></img>
-						<h4>상품명</h4>
-						<p>상품 설명 & 가격</p>
-					</div>
+					{shoe.map(function (i) {
+						let url = `https://codingapple1.github.io/shop/shoes${
+							i.id + 1
+						}.jpg`;
+
+						return (
+							<div className="col-md-4">
+								<img src={url} width="100%"></img>
+								<h4>{shoe[i.id].title}</h4>
+								<p>
+									{shoe[i.id].content} & {shoe[i.id].price}
+								</p>
+							</div>
+						);
+					})}
 				</div>
 			</div>
+		</div>
+	);
+}
+
+function ShoeCard(props) {
+	return (
+		<div className="col-md-4">
+			<img
+				src="https://codingapple1.github.io/shop/shoes1.jpg"
+				width="100%"
+			></img>
+			<h4>{props.shoe[0].title}</h4>
+			<p>
+				{props.shoe[0].content} & {props.shoe[0].price}
+			</p>
 		</div>
 	);
 }
